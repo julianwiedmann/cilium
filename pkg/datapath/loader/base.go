507ce90afb8d249daceef090586a797e8fcafd1f
Julian Wiedmann <jwi@isovalent.com>
2023-07-18 14:14:49 +0300

    datapath: untangle DSR-IPIP and L4LB health-check feature
    Refactor & rename a few things to prepare for health-check support with
    DSR-Geneve.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- pkg/datapath/loader/base.go
+++ pkg/datapath/loader/base.go
@@ -317,7 +317,8 @@ func (l *Loader) Reinitialize(ctx context.Context, o datapath.BaseProgramOwner, 
 	case option.Config.TunnelingEnabled():
 		mode = tunnelMode
 		encapProto = option.Config.TunnelProtocol
-	case option.Config.EnableHealthDatapath:
+	case option.Config.EnableHealthDatapath &&
+		option.Config.LoadBalancerDSRDispatch == option.DSRDispatchIPIP:
 		mode = option.DSRDispatchIPIP
 	default:
 		mode = directMode
